0662c4a0636d6fde3f827a98cf46a3cd136b05de
1992JYP <37524453+1992JYP@users.noreply.github.com>
2020-09-07 22:50:28 +0900

    Create githubtest.py
    first commit in github

---
+++ githubtest.py
@@ -0,0 +1,7 @@
+def solution(v):
+    x = v[2][0] if v[1][0] == v[0][0] else (v[1][0] if v[2][0] == v[0][0] else v[0][0])
+    y = v[2][1] if v[1][1] == v[0][1] else (v[1][1] if v[2][1] == v[0][1] else v[0][1])
+    answer = [x,y]
+
+
+    return answer
